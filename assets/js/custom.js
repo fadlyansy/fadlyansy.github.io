03774e6ef3cefba5e6999baf49a00b5bba6ad8e1
fadly <>
2022-01-12 19:55:04 +0700

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,18 +7,18 @@
 //   section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
 // })
 
-$(function() {
-    var rotation = 0.05, 
-        scrollLoc = $(document).scrollTop();
-    $(window).scroll(function() {
-        var newLoc = $(document).scrollTop();
-        var diff = scrollLoc - newLoc;
-        rotation += diff, scrollLoc = newLoc;
-        var rotationStr = "rotate(" + rotation + "deg)";
-        $(".gear").css({
-            "-webkit-transform": rotationStr,
-            "-moz-transform": rotationStr,
-            "transform": rotationStr
-        });
-    });
-})
+// $(function() {
+//     var rotation = 0.05, 
+//         scrollLoc = $(document).scrollTop();
+//     $(window).scroll(function() {
+//         var newLoc = $(document).scrollTop();
+//         var diff = scrollLoc - newLoc;
+//         rotation += diff, scrollLoc = newLoc;
+//         var rotationStr = "rotate(" + rotation + "deg)";
+//         $(".gear").css({
+//             "-webkit-transform": rotationStr,
+//             "-moz-transform": rotationStr,
+//             "transform": rotationStr
+//         });
+//     });
+// })
